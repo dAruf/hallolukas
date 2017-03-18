80c7e10add5099e58a42c02b940f474ca46309df
David <ruf@wort-une-web.de>
2017-03-18 11:03:40 +0100

    start

---
+++ src/server/render-app.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
+
+const renderApp = function (title) {
+  return(
+`<!doctype html>
+<html>
+  <head>
+    <title>${title}</title>
+    <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+  </head>
+  <body>
+    <div class="${APP_CONTAINER_CLASS}"></div>
+    <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+  </body>
+</html>
+`);
+}
+
+
+
+export default renderApp
